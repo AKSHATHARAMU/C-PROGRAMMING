d4a2f17cd4078ce256b905e374bcb2e3c408a981
AKSHATHARAMU <34640640+AKSHATHARAMU@users.noreply.github.com>
2017-12-18 12:55:03 +0530

    Create menu.c

---
+++ BEGINNER LEVEL/menu.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	char Name[50]="Sachin Ramesh Tendulkar",Nick[20]="Little Master";
+    int Runs=18426;
+    int aver=44.83;
+    int Mat=463;
+    printf("%s",Name);
+    printf("\n%s",Nick);
+    printf("\n%d",Runs);
+    printf("\n%d",aver);
+    printf("\n%d",Mat);
+}
