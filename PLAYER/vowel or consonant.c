b9a27d5e032d1370e0c7018a96992aa256adb8b5
AKSHATHARAMU <34640640+AKSHATHARAMU@users.noreply.github.com>
2017-12-18 20:35:31 +0530

    Create vowel or consonant.c

---
+++ PLAYER/vowel or consonant.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	char n;
+	printf("Enter an alphabet to check");
+	scanf("%c",&n);
+	if(n=='a'||n=='e'||n=='i'||n=='o'||n=='u'||n=='A'||n=='E'||n=='I'||n=='O'||n=='U')
+	{
+		printf("\nVowel");
+	}
+	else
+	printf("\nConsonant");
+return 0;
+}
