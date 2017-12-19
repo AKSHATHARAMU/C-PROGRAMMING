653298f6f9b941fbc89d1e49f887624124d24b0b
AKSHATHARAMU <34640640+AKSHATHARAMU@users.noreply.github.com>
2017-12-19 20:46:50 +0530

    Update repeating strings.c

--- CODEKATA/repeating strings.c
+++ CODEKATA/repeating strings.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
-void main()
+int main()
 {
 	int n,i;
-	printf("enter the number of times to repeat");
 	scanf("%d",&n);
 	for(i=0;i<n;i++)
 	{
 		printf("Hello");
 		printf("\n");
 	}
+return 0;
 }
