47bdb1afbf6ca3a74a4f023d96588c8d7615128a
AKSHATHARAMU <34640640+AKSHATHARAMU@users.noreply.github.com>
2017-12-19 20:29:57 +0530

    Update positive or negative.c

--- CODEKATA/positive or negative.c
+++ CODEKATA/positive or negative.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-void main()
+int main()
 {
 	int n;
 	scanf("%d",&n);
@@ -7,6 +7,11 @@ void main()
 	{
 		printf("\npositive");
 	}
+	else if(n==0)
+	{
+		printf("\n Zero");
+	}
 	else
 		printf("\nnegative");
+	return 0;
 }
