2befe06bbfae628b03f7e123d3dcc966fab80a90
AKSHATHARAMU <34640640+AKSHATHARAMU@users.noreply.github.com>
2017-12-21 20:27:19 +0530

    Update swap even and odd position.c

--- ASSIGNMENT/swap even and odd position.c
+++ ASSIGNMENT/swap even and odd position.c
@@ -1,15 +1,29 @@
 #include <stdio.h>
+
 int main(void) {
-	int i,len;
-	char a[10],temp;
-	gets(a);
-	len=strlen(a);
+	// your code goes here
+	char str[20],temp;
+	int len,i;
+	scanf("%s",str);
+	len=strlen(str);
+	if(len%2==0)
+	{
 	for(i=0;i<len;i+=2)
 	{
-		temp=a[i];
-		a[i]=a[i+1];
-		a[i+1]=temp;
+		temp=str[i];
+		str[i]=str[i+1];
+		str[i+1]=temp;
 	}
-	printf("%s",a);
-return 0;
+	}
+	else
+	{
+		for(i=0;i<len-1;i+=2)
+		{
+			temp=str[i];
+			str[i]=str[i+1];
+			str[i+1]=temp;
+		}
+	}
+	printf("%s",str);
+	return 0;
 }
