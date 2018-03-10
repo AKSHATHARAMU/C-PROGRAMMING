c64085be5a8aa35b829eaaf1e1db06061ee7c995
AKSHATHARAMU <34640640+AKSHATHARAMU@users.noreply.github.com>
2018-03-10 12:35:57 +0530

    Update number of lines in para.c

--- CODEKATA/number of lines in para.c
+++ CODEKATA/number of lines in para.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
+#include<string.h>
 int main(void) {
 	char str[100];
 	int i,count=0,len;
 	gets(str);
-	while(str[i]!='\0')
+	len=strlen(str);
+	for(i=0;i<=len;i++)
 	{
-		if(str[i]=='.')
+		if(str[i]=='.' || str[i]=='\0')
 		{
 		count=count+1;
 		}
-		i++;
 	}
 	printf("%d",count);
 	return 0;
 }
+
