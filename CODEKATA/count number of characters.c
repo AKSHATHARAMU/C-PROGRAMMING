de9a504f165443f613dd63aec3588f9b2c842ad5
AKSHATHARAMU <34640640+AKSHATHARAMU@users.noreply.github.com>
2018-03-10 12:08:40 +0530

    Update count number of characters.c

--- CODEKATA/count number of characters.c
+++ CODEKATA/count number of characters.c
@@ -5,12 +5,13 @@ int main(void) {
 	gets(str);
 	while(str[i]!='\0')
 	{
-		if(str[i]>='a' && str[i]<='z' || str[i]>='A' && str[i]<='Z')
-		{
-			count=count+1;
-		}
+		if(str[i]==' ')
+		str[i]=str[i+1];
+		else
+		count=count+1;
 		i++;
 	}
 	printf("%d",count);
 	return 0;
+}
 }
